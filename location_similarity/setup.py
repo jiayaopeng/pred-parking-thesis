fb36ecad5de3c455e1d5903b41460290d262ff5d
Jiayao Peng <prisc@macbook-pro.localdomain>
2021-09-16 17:53:10 +0200

    reformatted setup file

--- location_similarity/setup.py
+++ location_similarity/setup.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 
 def load_data_from_s3(bucket_name: str, file_path: str) -> pd.DataFrame:
-    client = boto3.client('s3')
+    client = boto3.client("s3")
     csv_obj = client.get_object(Bucket=bucket_name, Key=file_path)
-    body = csv_obj['Body']
-    csv_string = body.read().decode('utf-8')
+    body = csv_obj["Body"]
+    csv_string = body.read().decode("utf-8")
     data = pd.read_csv(io.StringIO(csv_string), index_col=0)
     return data
